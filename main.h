032253fbf47f5ace019982426417bdf2a9f71824
Lisahkamau <lisahkamau@gmail.com>
2023-11-14 02:40:18 -0800

    handle.d.i

--- main.h
+++ main.h
@@ -2,5 +2,11 @@
 #define MAIN_H
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_decimal(va_list args);
+int _printf(const char *format, ...);
+
 
 #endif
